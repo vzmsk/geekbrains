ac6bb0dd3cc1fd4fbb7c577629499fea3b8b0675
vzmsk <punishervictor@gmail.com>
2018-12-20 01:29:58 +0300

    1111

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by Виктор Жуков on 16.12.2018.
+//  Copyright © 2018 Виктор Жуков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello!\n";
+    return 0;
+}
